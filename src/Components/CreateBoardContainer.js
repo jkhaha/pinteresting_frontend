36d8655b391b485b1bf23d1bc98d63c7ef49d01b
github username <github email address>
2018-11-09 16:43:18 -0500

    added search form & fetch for search

--- src/Components/CreateBoardContainer.js
+++ src/Components/CreateBoardContainer.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import CurrentBoard from './CurrentBoard'
 import ImageOptions from './ImageOptions'
+import SearchForm from './SearchForm'
 
 class CreateBoardContainer extends Component{
+  state={
+    images:[]
+  }
+
+  performSearch=(e, query) =>{
+    e.preventDefault();
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=e67b6fb9adfed59b64dbe373ea44ecd2f315200fd108f1268a5b6fa0d66ad0f7`)
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+  }
+
   render(){
     return(
       <div>
       <h1>this is CreateBoardContainer (in WindowContainer)</h1>
+      <SearchForm performSearch={this.performSearch}/>
       <CurrentBoard/>
       <ImageOptions/>
       </div>
