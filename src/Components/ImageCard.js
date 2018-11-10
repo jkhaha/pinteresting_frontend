3e26817155b7d606a71cf2e3d165126d0715d25a
github username <github email address>
2018-11-09 23:14:59 -0500

    fixed bug and added thumbnail to ImageCard so it renders picture

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class ImageCard extends Component{
   render(){
     return(
+      <div>
       <h1>This is ImageCard (in ImageOptions & CurrentBoard)</h1>
+      <img src={this.props.thumbnail_url} alt=''/>
+      </div>
     )
   }
 }
